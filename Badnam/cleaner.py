b1360be20397ee93f0612cd26594bab5dee969dc
Agent_Op <101430495+DeadxEnD@users.noreply.github.com>
2022-05-08 20:43:20 +0530

    Update cleaner.py

--- Badnam/cleaner.py
+++ Badnam/cleaner.py
@@ -20,9 +20,9 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("✅ **removed all downloaded files**")
+        await message.reply_text("✅ **Rᴇᴍᴏᴠᴇ Aʟʟ Dᴏᴡɴʟᴏᴀᴅ Fɪʟᴛᴇʀꜱ**")
     else:
-        await message.reply_text("❌ **no files is downloaded**")
+        await message.reply_text("❌ **Nᴏ Fɪʟᴇ Iꜱ Dᴏᴡɴʟᴏᴀᴅᴇᴅ**")
 
 
 @Client.on_message(command(["clear", f"clear@{BOT_USERNAME}"]) & ~filters.edited)
@@ -34,6 +34,6 @@ async def clear_jpg_image(_, message: Message):
     if ls_dir:
         for dta in os.listdir(pth):
             os.system("rm -rf *.jpg")
-        await message.reply_text("✅ **succesfully cleared**")
+        await message.reply_text("✅ **Cʟᴇᴀʀᴅ Sᴜᴄᴄᴇꜱ**")
     else:
-        await message.reply_text("✅ **already cleared**")
+        await message.reply_text("✅ **Aʟʀᴇᴀᴅʏ Cʟᴇᴀʀᴅ**")
